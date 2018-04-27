4d48a89b22edc0d32d8982db390070fd35dcb43a
Billy Vong <billy@sentry.io>
2018-04-26 18:23:15 -0700

    fix cookie logic

--- src/sentry/static/sentry/app/actionCreators/sidebar.jsx
+++ src/sentry/static/sentry/app/actionCreators/sidebar.jsx
@@ -6,14 +6,14 @@ const SIDEBAR_COOKIE_KEY = 'sidebar_collapsed';
 
 export function hideSidebar() {
   SidebarActions.hideSidebar();
-  Cookies.set(SIDEBAR_COOKIE_KEY, 1);
+  Cookies.set(SIDEBAR_COOKIE_KEY, '1');
 }
 
 export function showSidebar() {
   SidebarActions.showSidebar();
-  Cookies.set(SIDEBAR_COOKIE_KEY, 0);
+  Cookies.set(SIDEBAR_COOKIE_KEY, '0');
 }
 
 export function loadSidebarState() {
-  SidebarActions.loadInitialState(!!Cookies.get(SIDEBAR_COOKIE_KEY));
+  SidebarActions.loadInitialState(Cookies.get(SIDEBAR_COOKIE_KEY) === '1');
 }
